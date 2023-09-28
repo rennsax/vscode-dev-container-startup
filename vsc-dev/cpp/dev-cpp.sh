cf9efc4d9a12ee368bcde90df6b051e3e3eab919
Bojun Ren <bj_Ren@outlook.com>
2023-09-28 16:12:05 +0800

    enhancement
    Add env: `DEBIAN_FRONTEND=noninteractive`.
    
    Enable options for building the newest llvm
        when building vsc-dev/cpp:
    - DEV_CPP_BUILD_LLVM
    - DEV_CPP_CACHE_REMOVE
    - DEV_CPP_INSTALL_PREFIX

--- vsc-dev/cpp/dev-cpp.sh
+++ vsc-dev/cpp/dev-cpp.sh
@@ -9,6 +9,8 @@ set -e
 : ${DEV_CPP_CACHE_REMOVE:=true}
 # Where to install other build system, for example llvm
 : ${DEV_CPP_INSTALL_PREFIX:=/opt}
+# Whether to **build** newest llvm
+: ${DEV_CPP_BUILD_LLVM:=0}
 
 if [ $(id -u -n) != "root" ]; then
     echo "Please run this script as the root" >&2
@@ -16,23 +18,28 @@ if [ $(id -u -n) != "root" ]; then
 fi
 
 # Make sure indispensable packages have been installed
-apt update
-apt install build-essential make cmake gdb -y
+apt-get update
+apt-get install build-essential make cmake gdb -y
+apt-get install clang clang-tidy clang-format lldb lld -y
+# TODO `lldb --version` bug
 rm -rf /var/lib/apt/lists/*
 
-if [[ $INSTALL_LLVM != 1 ]]; then
+if [[ $DEV_CPP_BUILD_LLVM != 1 ]]; then
     exit 0
 fi
 
-# If $INSTALL_LLVM = 1, install llvm
+# If $INSTALL_LLVM = 1, build llvm
+# https://llvm.org/docs/CMake.html
 git clone --depth=1 https://github.com/llvm/llvm-project.git \
     $DEV_CPP_INSTALL_PREFIX/cellar/llvm-project
-mkdir -p $DEV_CPP_INSTALL_PREFIX/cellar/llvm-project/build
-cd $DEV_CPP_INSTALL_PREFIX/cellar/llvm-project/build
-cmake -DLLVM_ENABLE_PROJECTS="clang;lldb;clang-tools-extra"\
-         -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$DEV_CPP_INSTALL_PREFIX/llvm \
-         -G "Unix Makefiles" ../llvm
-make install -j4 # Very very long...
+# mkdir -p $DEV_CPP_INSTALL_PREFIX/cellar/llvm-project/build
+cd $DEV_CPP_INSTALL_PREFIX/cellar/llvm-project
+cmake -S llvm -B build -G "Unix Makefiles" \
+         -DLLVM_ENABLE_PROJECTS="clang;extra-tools;lldb;lld;clang-tools-extra" \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DCMAKE_INSTALL_PREFIX=$DEV_CPP_INSTALL_PREFIX/llvm
+cmake --build build -j4
+cmake --install build # Very very long...
 
 # If $PATH doesn't contain `${DEV_CPP_INSTALL_PREFIX}/llvm/bin`
 if [[ $PATH != *$DEV_CPP_INSTALL_PREFIX/llvm/bin* ]]; then
